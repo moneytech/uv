a0610986e30fc3960794b2d6c2955d3426d3fb8e
Joseph Werle <joseph.werle@gmail.com>
2019-08-01 11:20:59 -0400

    fix(clib-install.sh): Ensure 'PREFIX' can be resolved

--- clib-install.sh
+++ clib-install.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+CWD=$PWD
+
+cd -
+LWD=$PWD
+
 if [ -z "${PREFIX}" ]; then
   PREFIX=/usr/local
 fi
 
+mkdir -p $PREFIX
+cd $PREFIX
+PREFIX=$PWD
+cd $CWD
+
 ## make !
 {
   sh autogen.sh
-  ./configure --prefix="$PREFIX"
+  ./configure --prefix=$PREFIX
   make
   make check
   make install
